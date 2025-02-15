920b816dfd5b0df2d91d7685302b2a05effc4d95
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-02-15 12:28:05 +0530

    update base path comment in Vite configuration for clarity

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/30_Projects_React/',
+  base: '/30_Projects_React/', // This should match your repo name
 })
