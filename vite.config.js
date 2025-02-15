e91dd0d85081b531756ecd7db9836e3a0ac1d626
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-02-15 15:42:02 +0530

    update base path in Vite configuration to match repository name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/30_Projects_React/', // This should match your repo name
+  base: '/30_React_Projects/', // This should match your repo name
 })
